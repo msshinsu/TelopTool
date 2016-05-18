81d565e39f5d87b247a4245f714c285848d8743f
tsuka <tsuka@D-0041W2G>
2016-05-18 17:35:38 +0900

    プロジェクト ファイルを追加します。

---
+++ TelopTool/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace TelopTool
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // マウス移動
+            this.MouseLeftButtonDown += (sender, e) => { this.DragMove(); };
+            this.MouseEnter += (sender, e) => { this.Background = new SolidColorBrush(Color.FromArgb(10, 200, 200, 200)); };
+            this.MouseLeave += (sender, e) => { this.Background = new SolidColorBrush(Colors.Transparent); };
+
+        }
+    }
+}
